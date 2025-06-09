67a2de46f5829deee8aa303127f0af3a8d356c9a
gialocdev <locpg12@gmail.com>
2025-06-09 14:36:33 +0700

    off eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   eslint: {
+    ignoreDuringBuilds: true, // tắt eslint khi build production
+  },
   images: {
     domains: ['via.placeholder.com'], // 👈 thêm domain này
   },
